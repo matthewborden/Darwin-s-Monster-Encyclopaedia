094c59d027aa0433a02d890c18730038688d5a2e
Matthew Borden <matthew.borden@stileeducation.com>
2017-04-03 16:31:29 +1000

    Initial Commit

---
+++ Darwin’s Monster Encyclopaedia/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Darwin’s Monster Encyclopaedia
+//
+//  Created by Matthew Borden on 3/4/17.
+//  Copyright © 2017 Matthew Borden. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
